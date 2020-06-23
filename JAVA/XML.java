21ef5ec67ce8224155e51680096e890430550afb
Robert Kagan <45124869+libalis@users.noreply.github.com>
2020-06-23 18:58:10 +0200

    Add files via upload

--- JAVA/XML.java
+++ JAVA/XML.java
@@ -1,11 +1,12 @@
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 
+import org.w3c.dom.NodeList;
 import org.w3c.dom.Document;
+
 import java.io.File;
 
 public class XML {
-    public int length;
     public String[] id;
     public String[] name;
     public String[] amount;
@@ -17,6 +18,8 @@ public class XML {
     private DocumentBuilder documentBuilder;
     private Document document;
 
+    public int length;
+
     public XML() {
         try {
             file = new File("database.xml");
@@ -25,47 +28,46 @@ public class XML {
             document = documentBuilder.parse(file);
         } catch(Exception exception) {}
 
-        String test[] = new String[999];
-        int tmp = 0;
-
-        try {
-            for(tmp = 0; tmp<999; tmp++) {
-                test[tmp] = document.getElementsByTagName("ID").item(tmp).getTextContent();
-
-            }
-        } catch(Exception exception) {length = tmp;}
+        length = document.getElementsByTagName("ID").getLength();
 
         id = new String[length];
         name = new String[length];
         amount = new String[length];
         category = new String[length];
         bestbefore = new String[length];
-    }
 
-    public void readOut() {
-        for(int tmp = 0; tmp<length; tmp++) {
-            id[tmp] = document.getElementsByTagName("ID").item(tmp).getTextContent();
-            name[tmp] = document.getElementsByTagName("Name").item(tmp).getTextContent();
-            amount[tmp] = document.getElementsByTagName("Amount").item(tmp).getTextContent();
-            category[tmp] = document.getElementsByTagName("Category").item(tmp).getTextContent();
-            bestbefore[tmp] = document.getElementsByTagName("BestBefore").item(tmp).getTextContent();
-        }
-    }
-
-    public void liveDemo() {
         readOut();
-        for(int tmp = 0; tmp<length; tmp++) {
-            System.out.println("ID: " + id[tmp]);
-            System.out.println("Name: " + name[tmp]);
-            System.out.println("Amount:" + amount[tmp]);
-            System.out.println("Category:" + category[tmp]);
-            System.out.println("BestBefore:" + bestbefore[tmp]);
-            System.out.println();
-        }
+    }
+
+    public int readOut() {
+        try {
+            for(int tmp = 0; tmp<length; tmp++) {
+                id[tmp] = document.getElementsByTagName("ID").item(tmp).getTextContent();
+                name[tmp] = document.getElementsByTagName("Name").item(tmp).getTextContent();
+                amount[tmp] = document.getElementsByTagName("Amount").item(tmp).getTextContent();
+                category[tmp] = document.getElementsByTagName("Category").item(tmp).getTextContent();
+                bestbefore[tmp] = document.getElementsByTagName("BestBefore").item(tmp).getTextContent();
+            }
+            return 0;
+        } catch(Exception exception) {return 1;} //process failed
+    }
+
+    public int printOut() {
+        try {
+            for(int tmp = 0; tmp<length; tmp++) {
+                System.out.println("ID: " + id[tmp]);
+                System.out.println("Name: " + name[tmp]);
+                System.out.println("Amount: " + amount[tmp]);
+                System.out.println("Category: " + category[tmp]);
+                System.out.println("BestBefore: " + bestbefore[tmp]);
+                System.out.println();
+            }
+            return 0;
+        } catch(Exception exception) {return 1;} //process failed
     }
 
     public static void main(String args[]) {
         XML main = new XML();
-        main.liveDemo();
+        main.printOut();
     }
 }
