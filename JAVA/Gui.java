843c4607a9a4b59c68bd1f477722e2117511f83d
marcelrose1 <65225744+marcelrose1@users.noreply.github.com>
2020-06-26 09:29:38 +0200

    Add files via upload

--- JAVA/Gui.java
+++ JAVA/Gui.java
@@ -1,5 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.ImageIcon;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Gui {
     public static void main(String args[]){
@@ -8,19 +14,27 @@ public class Gui {
         JPanel ContentPane =new JPanel();
         ContentPane.setLayout(Layout);
         ContentPane.setBackground(Color.WHITE);
-        JLabel Headerlabel =new JLabel("Schulsanitätsdienst Materialmanegement", SwingConstants.CENTER);
+        JLabel Headerlabel =new JLabel("Schulsanitätsdienst Materialmanegement");
+
+
+        
+        ImageIcon image = new ImageIcon("C:\\Users\\Meneer\\Pictures\\image.png");
+        JLabel imageLabel = new JLabel(image);
+
+
+
         Headerlabel.setBackground(Color.red);
 
         Headerlabel.setFont(new Font("Sans", Font.BOLD, 30));
 
 
         ContentPane.add(Headerlabel);
-
+        f.setSize(1000,1000);
         Layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, Headerlabel, 0, SpringLayout.HORIZONTAL_CENTER, ContentPane);
         f.add(ContentPane);
         f.setVisible(true);
         f.setContentPane(ContentPane);
-        f.setSize(1000,1000);
+
         f.setLayout(null);
         f.setVisible(true);
     }
