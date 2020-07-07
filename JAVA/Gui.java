bf1b14a7906dd86b650b4e9c4342c8fd45a227a8
marcelrose1 <65225744+marcelrose1@users.noreply.github.com>
2020-07-07 12:05:41 +0200

    Button Produkte hinzufügen
    Ein neuer button "Produkte hinzufügen" mit dem Namen Create_New wurde hinzugefügt. Dazu ein Action Listener, welcher später eine andere Klasse zum hinzufügen von Produkten in einem Dialog startet.

--- JAVA/Gui.java
+++ JAVA/Gui.java
@@ -4,10 +4,12 @@ import javax.swing.ImageIcon;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Gui {
+public class gui{
     public static void main(String args[]){
         JFrame f=new JFrame();
         SpringLayout Layout = new SpringLayout();
@@ -15,22 +17,29 @@ public class Gui {
         ContentPane.setLayout(Layout);
         ContentPane.setBackground(Color.WHITE);
         JLabel Headerlabel =new JLabel("Schulsanitätsdienst Materialmanegement");
+        JButton Create_New = new JButton("Produkte hinzufügen");
 
 
-        
+
         ImageIcon image = new ImageIcon("C:\\Users\\Meneer\\Pictures\\image.png");
         JLabel imageLabel = new JLabel(image);
 
-
+        Create_New.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    System.out.println("Wurde gedrückt");
+            }
+        });
 
         Headerlabel.setBackground(Color.red);
 
         Headerlabel.setFont(new Font("Sans", Font.BOLD, 30));
 
-
+        ContentPane.add(Create_New);
         ContentPane.add(Headerlabel);
-        f.setSize(1000,1000);
+        f.setSize(1280,720);
         Layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, Headerlabel, 0, SpringLayout.HORIZONTAL_CENTER, ContentPane);
+        Layout.putConstraint(SpringLayout.WEST, Create_New, 100, SpringLayout.WEST, ContentPane);
+        Layout.putConstraint(SpringLayout.NORTH, Create_New, 50, SpringLayout.NORTH, ContentPane);
         f.add(ContentPane);
         f.setVisible(true);
         f.setContentPane(ContentPane);
